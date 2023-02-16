ffb3149417357a5c74ffb7f7824992471cb43a97
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-16 13:17:57 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Description: 'the program's description'
+* printing using 'printf'
 * Return: Always 0 (Success)
 */
 int main(void)
-{ /* Displays the string inside quotations */
-printf("with proper grammar, but the outcome is a piece of art,");
-printf("");
+{ /* printf */
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
