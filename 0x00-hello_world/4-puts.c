f1ebea1e340ac946b3137ba712ef7201f175cb7c
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-16 12:52:24 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 #include <string.h>
 /* printing exactly using puts */
 int main(void)
-{
+{ /* using puts command */
 char str[] = "\"Programming is like building a multilingual puzzle";
-/* using puts command */
 puts(str);
 return (0); }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{ /* Displays the string inside quotations */
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0); }
