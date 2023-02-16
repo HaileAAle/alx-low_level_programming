86a4dc4dc722df91021d32cc4e646b7d65729ee9
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-16 14:13:15 -0800

    computer size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long longType;
 char charType;
 long long longlongType;
 /* sizeof evaluates the size of a variable */
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long: %zu bytes\n", sizeof(longType));
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of long long: %zu byte\n", sizeof(longlongType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a float: %zu bytes\n", sizeof(floatType));
+printf("Size of a long int: %zu bytes\n", sizeof(longType));
+printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of a long long int: %zu byte\n", sizeof(longlongType));
 return (0); }
