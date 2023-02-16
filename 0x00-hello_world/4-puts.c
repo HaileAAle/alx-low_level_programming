4b2b297f56a13853fc655eacf3e76dbaba3e5ac5
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-16 11:48:31 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) 
+int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  
-  puts(str);
-
-  return 0;
-}
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return 0;}
