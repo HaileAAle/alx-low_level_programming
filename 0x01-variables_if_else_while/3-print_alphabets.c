f9fc4718208d78993d002527434ebf3d63c7c1ef
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-17 12:16:50 -0800

    print alphabtes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* display alphabetes
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+		putchar(n++);
+	n = 65;
+	while (n <= 90)
+		putchar(n++);
+	putchar(10);
+	return (0);
+}
