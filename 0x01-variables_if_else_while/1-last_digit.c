660e7848b64d67899af3214a011453c50dc9acb4
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-17 07:30:49 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,17 +9,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-if( n == 0 ) {
-	/* if n is zero */
-	printf("%d", n ); 
-	printf(" and is 0" );
-} else if( n > 5 ) {
-	/* if n is greater than 5 */
-	printf("%d", n );
-	printf(" and is greater than 5" );
-} else {
-	/* if n is less than 6 */
-	printf("%d", n );
-	printf(" and is less than 6 and not 0" );
+	
+	if( n == 0 )
+	{
+		printf("%d", n ); 
+		printf(" and is 0" );
+	} else if( n > 5 )
+	{
+		printf("%d", n );
+		printf(" and is greater than 5" );
+	} else
+	{
+		printf("%d", n );
+		printf(" and is less than 6 and not 0" );
 	} 
-return (0); }
+	return (0); 
+}
