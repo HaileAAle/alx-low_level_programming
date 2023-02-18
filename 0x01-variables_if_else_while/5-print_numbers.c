ea278781addc538dbcded725d2eef7086b7152cb
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-18 01:48:40 -0800

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* print single digit numbers
+* return 0 (success)
+*
+*/
+int main(void)
+{
+	int n = '0';
+	while ( n <= '9')
+		putchar (n++);
+	putchar(10);
+	return(0);
+}
