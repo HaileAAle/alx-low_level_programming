350c3148f707d4308c560085b2fd058fc9aa8186
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-21 21:47:41 -0800

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_X10 times - Prints the alphabet in lowercase.
+ * print_alphabet_x10 times - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
