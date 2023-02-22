5cf7fba5f891ab1371994b1f4d0eb60ddca540d0
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-22 02:50:18 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,26 @@
 #include "main.h"
 
 /**
+ * print_last_digit - Prints the last digit
+ * of a number.
  *
+ * @n: input number as an integer.
  *
- *
- *
- *
+ * Return: last digit.
  */
-int print_last_digit(int i)
+int print_last_digit(int n)
 {
-	i = i % 10;
-	return (i);
-putchar('\n');
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
 }
