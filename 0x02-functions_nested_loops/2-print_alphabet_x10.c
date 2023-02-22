9bdacb2c1ecbcb17f3e265c0d1b49be4758fe416
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-21 21:26:55 -0800

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,12 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i=0; i<=9; i++)
 	{
-		_putchar(i'\n', i'\n', i'\n', i'\n', i'\n', i'\n', i'\n', i'\n', i'\n', i'\n');
+		for (i = 97; i <= 122; i++)
+		{	
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
