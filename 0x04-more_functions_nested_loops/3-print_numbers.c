4b0cd3d7da8081414ee7ad917a743bab5eb86ba8
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-23 11:48:14 -0800

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i >= 48 && i <= 57)
+	{
+		_putchar(i++);
+	}
+	_putchar('\n');
+}
