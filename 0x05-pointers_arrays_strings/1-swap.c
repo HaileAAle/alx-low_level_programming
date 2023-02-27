75b229a2f39ec228de28cfe97e5d8253ad9f44a2
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-02-27 14:08:05 -0800

    swap twp numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swaps two intergers 
+ * swap_int - swaps two intergers
  * @a: first integer
  * @b: second integer
  * Return: void
- *
  */
 
 void swap_int(int *a, int *b)
 {
+	int c = *a;
+	
 	*a = *b;
-	*b = *a;
+	*b = c;
 }
