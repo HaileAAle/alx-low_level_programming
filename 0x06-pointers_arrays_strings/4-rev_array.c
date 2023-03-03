daf81ab9a160231a68d467e3ec3bcb0101c4d8bb
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-03 15:20:47 -0800

    revers_arrat

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,9 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i = 0;
+		int i, j, temp;
 
-	while (i <= n)
-		_putchar("%d", *a[n - i]);
-	i++;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
