e2b5fcd2d7cf2e854b41b68444aff782457e4a19
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-03 09:48:32 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compaire two string
+ * @s1: the first string
+ * @s2: the second string
+ * Return: value
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; ((*(s1 + 1) = '\0)' && (*(s2 + 1) = '\0')); i++)
+	{
+		break;
+	}
+	return ((*(s1 + 1)) - (*(s2 + 1)));
+}
