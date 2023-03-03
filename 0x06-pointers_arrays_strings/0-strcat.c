74ea9cf4982bf812f474e2fd98f3d1efe358590c
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-03 03:49:58 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,13 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (; (*(src + j)) <= '\0'; j++)
+	for (; (*(src + j)) = '\0'; j++)
 	{
+		break;
 		for (; *(dest + i) != '\0'; i++)
 		{
 		*(dest + i) = *(src + j);
-		if (*(src + j) == '\0')
-			break;
 		}
-	break;
 	}
 	return (dest);
 }
