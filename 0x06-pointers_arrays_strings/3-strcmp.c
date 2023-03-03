94e1a79930cdb013cb0c8e28b08d18c4e25d2ce8
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-03 09:57:24 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; ((*(s1 + i) = '\0)' && (*(s2 + i) = '\0')); i++)
-	
+	while ((*(s1 + i) = '\0)' && (*(s2 + i) = '\0'))
+
 		break;
+	i++;
 	return ((*(s1 + i)) - (*(s2 + i)));
 }
