d1fa6394bcc4e369f0fdff2cad45873b05f286d5
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 04:01:40 -0800

    multiplies two number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,11 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc = 3)
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
-	else
+	if (argc != 3)
 	{
 		printf("error\n");
 		return (0);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
