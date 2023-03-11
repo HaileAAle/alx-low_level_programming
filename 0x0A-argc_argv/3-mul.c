06b3df58e63a5d329f5b11a75d9b6a4c667ec115
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 03:46:05 -0800

    multipilies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: argument counter
+ * @argv: two numbers
+ * Return: 0 = success, 1 = error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc = 2)
+		printf("%d\n", argv[1] * argv[2]);
+	else
+		printf("error\n");
+		return (0);
+	return (0);
+}
