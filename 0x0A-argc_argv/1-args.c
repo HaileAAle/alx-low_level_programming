6c5e9fc04d10022ce8e832863fb06f08972f1300
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 02:54:08 -0800

    number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  *
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	while (argv++)
-		printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
