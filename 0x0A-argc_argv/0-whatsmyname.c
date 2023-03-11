42eba72eb99839b5859ef33065c5d6daa18e26b9
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 00:55:22 -0800

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main -  prints its name, followed by a new line.
+ * Return: no return
+ *
+ */
+
 int main(int argc, char *argv[])
 {
-	_putchar('%s'\n'', **argv);
+	_putchar("%s'\n'", **argv);
+	
 	return (0);
 }
