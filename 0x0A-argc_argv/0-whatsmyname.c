33eeaf6c3c3f5b15a35e5561c88768de513c75ab
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 01:02:09 -0800

    argc, argv argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main -  prints its name, followed by a new line.
+ * @argc: count argument passed to argv
+ * @argv: the argument character
  * Return: no return
- *
  */
 
 int main(int argc, char *argv[])
 {
-	_putchar("%s'\n'", **argv);
-	
+	_putchar("%s\n", argv[0]);
 	return (0);
 }
