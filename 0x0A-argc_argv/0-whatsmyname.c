d8b8d19ec1174e8496778b045795c12fc0818f6c
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-11 01:13:59 -0800

    argc, argv argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * Return: no return
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar("%s\n", argv[0]);
+	_putchar("%s\n", *argv[0]);
 	return (0);
 }
