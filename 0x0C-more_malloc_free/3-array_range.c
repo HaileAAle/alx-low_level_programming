b147a341c15711a5c4e2f448e34ad7c876aaefa1
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-16 05:38:38 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,11 +14,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	p = malloc(sizeof(int) * max - min +1);
+	p = malloc(sizeof(int) * (max - min + 1));
 
-	for (i = 0; i <= max - min; i++)
-		p[i++] = min + i++;
-	if (p == NULL)
-		return (NULL);
+	for (i = 0; i <= (max - min + 1); i++)
+		p[i++] = min++;
+	
+	if (!p)
+		return (0);
 	return (p);
 }
