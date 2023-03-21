cdfcd0de5643084d829c8bf2420ba4b7df3c24a0
HaileAAle <delbante.alebachew@aastu.edu.et>
2023-03-21 12:03:10 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,20 @@
+#include "dog.h"
 
 /**
- * dog - initializes a variable of type struct dog.
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: owner of the dog.
  *
  * Return: no return.
  */
-
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
